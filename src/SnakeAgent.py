e4f0d81c4b491d18b21c4f70b12f28c802074378
Armandase <damiens.armand@gmail.com>
2024-11-04 18:13:19 +0100

    J'AI DU WIFI

--- src/SnakeAgent.py
+++ src/SnakeAgent.py
@@ -25,15 +25,17 @@ class SnakeAgent():
     def update_epsilon(self):
         self.eps = max(self.min_eps, self.eps * self.eps_discount)
 
-    def update_q_table(self, state, action, reward, next_state):
-        if self.prev_state is not None:
-            cumulative_reward = self.discount_rate * np.max(self.q_table[np.astype(next_state, np.int32)])
-            calcul = self.learning_rate * (
-                reward + cumulative_reward - self.q_table[np.astype(self.prev_state, np.int32), self.prev_action]
-            )
-            self.q_table[np.astype(self.prev_state, np.int32), self.prev_action] += calcul
-
-        self.prev_state = state
+    def update_q_table(self, current_state, action, reward, next_state):
+        # if self.prev_state is not None:
+        #     cumulative_reward = self.discount_rate * np.max(self.q_table[np.astype(next_state, np.int32)])
+        #     calcul = self.learning_rate * (
+        #         reward + cumulative_reward - self.q_table[np.astype(self.prev_state, np.int32), self.prev_action]
+        #     )
+        #     self.q_table[np.astype(self.prev_state, np.int32), self.prev_action] += calcul
+        # self.prev_state = state
+        self.q_table[current_state][action] = (1 - self.learning_rate)\
+                            * self.q_table[np.astype(current_state, np.int32)][action] + self.learning_rate\
+                            * (reward + self.discount_rate * max(self.q_table[np.astype(next_state, np.int32)]))
         self.prev_action = action
         self.score += reward
         self.steps += 1
@@ -46,14 +48,15 @@ class SnakeAgent():
         for epoch in range(epochs):
             self.reset()
             grid.reset_grid()
-            state = game_state(grid)
+            self.state = game_state(grid)
             done = False
             while not done:
-                action = self.get_action(state, self.q_table)
+                action = self.get_action(self.state, self.q_table)
                 reward, done = step(action, grid)
                 next_state = game_state(grid)
-                self.update_q_table(state, action, reward, next_state)
-                state = next_state
+                # self.update_q_table(state, action, reward, next_state)
+                self.update_q_table(self.state, action, reward, next_state)
+                self.state = next_state
             self.update_epsilon()
             print(f"Epoch {epoch + 1}/{epochs}")
             print(f"Score: {self.score}")
